4d1973ea436e7fc54fdde9eee499df9f5ca657c0
tchallaya <2569303197@qq.com>
2023-02-18 12:57:29 +0800

    第一次提交代码

--- test0_1.c
+++ test0_1.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int max(int x, int y);
- max()函数声明
+ max()  //函数声明
 void main(){
     int a, b, m;
     scanf_s("%d,%d", &a, &b);
     m = max(a, b);
-    用户自定义函数的调用
+    //用户自定义函数的调用
     printf("a = %d,b = %d,m = %d", a, b, m);
 }
 int max(int x, int y){
-    用户自定义函数，求两个数中的较大值
+    //用户自定义函数，求两个数中的较大值
     int m;
     if (x > y)
         m = x;
